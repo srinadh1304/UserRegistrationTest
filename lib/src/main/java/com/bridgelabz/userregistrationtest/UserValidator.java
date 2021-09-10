1b407c5eb0d6206a08fa68c9ea4538536372ea88
srinadh1304 <srinadh1304@gmail.com>
2021-09-10 19:58:52 +0530

    ADDED: Method to validate name

--- lib/src/main/java/com/bridgelabz/userregistrationtest/UserValidator.java
+++ lib/src/main/java/com/bridgelabz/userregistrationtest/UserValidator.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.userregistrationtest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserValidator {
-	public static void main(String[] args) {
-		System.out.println("Welcome to user validator program");
+	
+	public static boolean validateName(String name) {
+		if(name == null) {
+			return false;
+		}
+		if(name == "") {
+			return false;
+		}
+		String nameValidation="^[A-Z][a-z]{2,}$";
+		return Pattern.matches(nameValidation, name);
 	}
 }
