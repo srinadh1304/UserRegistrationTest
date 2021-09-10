e9ef4863f02c60d89519410abd389cb581caf350
srinadh1304 <srinadh1304@gmail.com>
2021-09-10 20:13:54 +0530

    ADDED: Methods to test validation of Last name

--- lib/src/test/java/com/bridgelabz/userregistrationtest/UserValidatorTest.java
+++ lib/src/test/java/com/bridgelabz/userregistrationtest/UserValidatorTest.java
@@ -55,4 +55,55 @@ public class UserValidatorTest {
 		Assert.assertFalse(isNotValid);
 		
 	}
+	
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		UserValidator validator = new UserValidator();	
+		boolean result = validator.validateName("Tadiparthi");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse() {
+		UserValidator validator = new UserValidator();	
+		boolean result = validator.validateName("ta");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenLastName_WhenUpperCaseMissing_ShouldReturnFalse() {
+		UserValidator validator = new UserValidator();	
+		boolean result = validator.validateName("tadiparthi");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenLastName_WhenNumbers_ShouldReturnFalse() {
+		UserValidator validator = new UserValidator();	
+		boolean result = validator.validateName("Fdjs134");
+		Assert.assertFalse(result);
+	}
+	@Test
+	public void givenLastName_WhenNull_ShouldReturnFalse() {
+		UserValidator userValidator = new UserValidator();
+		boolean isNotValid = userValidator.validateName(null);
+		Assert.assertFalse(isNotValid);
+		
+	}
+	
+	@Test
+	public void givenLastName_WhenEmpty_ShouldReturnFalse() {
+		UserValidator userValidator = new UserValidator();
+		boolean isNotValid = userValidator.validateName("");
+		Assert.assertFalse(isNotValid);
+		
+	}
+	
+	@Test
+	public void givenLastName_WhenSpecialCharatersPresent_ShouldReturnFalse() {
+		UserValidator userValidator = new UserValidator();
+		boolean isNotValid = userValidator.validateName("fdsa@^");
+		Assert.assertFalse(isNotValid);
+		
+	}
 }
