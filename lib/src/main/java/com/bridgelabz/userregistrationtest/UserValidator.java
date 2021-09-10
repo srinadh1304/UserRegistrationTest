b6542391179e493c31f8b5a402e1f6917d6b4937
srinadh1304 <srinadh1304@gmail.com>
2021-09-10 20:00:09 +0530

    ADDED: Method to validate email

--- lib/src/main/java/com/bridgelabz/userregistrationtest/UserValidator.java
+++ lib/src/main/java/com/bridgelabz/userregistrationtest/UserValidator.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-	
+
 	public static boolean validateName(String name) {
 		if(name == null) {
 			return false;
@@ -13,5 +13,18 @@ public class UserValidator {
 		}
 		String nameValidation="^[A-Z][a-z]{2,}$";
 		return Pattern.matches(nameValidation, name);
+	}
+
+	public boolean validateEmail(String email) {
+
+		if(email == null) {
+			return false;
+		}
+		if(email == "") {
+			return false;
+		}
+		Pattern pattern = Pattern.compile("([a-zA-Z][a-zA-Z0-9_-]*([^.]?$)*[a-zA-Z0-9])(([+_.-][a-zA-Z0-9]*)?)(@[a-zA-Z0-9]+)([.])([a-z]{2,})(([.][a-z]{2,})?)");
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
 	}
 }
