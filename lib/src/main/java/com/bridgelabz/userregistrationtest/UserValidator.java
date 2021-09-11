d0e9ae847493779b592b54c68ff797b59012aed1
srinadh1304 <srinadh1304@gmail.com>
2021-09-11 19:03:50 +0530

    REFACTORED: throwing uservalidation exception using enum

--- lib/src/main/java/com/bridgelabz/userregistrationtest/UserValidator.java
+++ lib/src/main/java/com/bridgelabz/userregistrationtest/UserValidator.java
@@ -2,16 +2,18 @@ package com.bridgelabz.userregistrationtest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.bridgelabz.userregistrationtest.UserValidationException.ExceptionType;
+
 public class UserValidator {
 
 	public static boolean validateName(String name)  throws UserValidationException {
 		try {
 			if (name.length() == 0) {
-				throw new UserValidationException("Please Enter Name. No Name entered.");
+				throw new UserValidationException(ExceptionType.ENTERED_EMPTY,"Please Enter Name. No Name entered.");
 			}
 		}
 		catch (NullPointerException e) {
-			throw new UserValidationException("Please enter valid name. Entered Null.");
+			throw new UserValidationException(ExceptionType.ENTERED_NULL,"Please enter valid name. Entered Null.");
 		}
 		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
 		Matcher matcher=pattern.matcher(name);
@@ -21,11 +23,11 @@ public class UserValidator {
 	public static boolean validateEmail(String email) throws UserValidationException {
 		try {
 			if (email.length() == 0) {
-				throw new UserValidationException("Please Enter Email. No Email entered.");
+				throw new UserValidationException(ExceptionType.ENTERED_EMPTY,"Please Enter Email. No Email entered.");
 			}
 		}
 		catch (NullPointerException e) {
-			throw new UserValidationException("Please enter valid email. Entered Null.");
+			throw new UserValidationException(ExceptionType.ENTERED_NULL,"Please enter valid email. Entered Null.");
 
 		}
 		Pattern pattern = Pattern.compile("([a-zA-Z][a-zA-Z0-9_-]*([^.]?$)*[a-zA-Z0-9])(([+_.-][a-zA-Z0-9]*)?)(@[a-zA-Z0-9]+)([.])([a-z]{2,})(([.][a-z]{2,})?)");
@@ -36,11 +38,11 @@ public class UserValidator {
 	public boolean validateMobileNumber(String mobileNumber)throws UserValidationException {
 		try {
 			if (mobileNumber.length() == 0) {
-				throw new UserValidationException("Please Enter Valid Mobile Number. No Mobile Number Entered.");			
+				throw new UserValidationException(ExceptionType.ENTERED_EMPTY,"Please Enter Valid Mobile Number. No Mobile Number Entered.");			
 			}
 		}
 		catch (NullPointerException e) {
-			throw new UserValidationException("Please Enter Valid Mobile Number.Entered null.");
+			throw new UserValidationException(ExceptionType.ENTERED_NULL,"Please Enter Valid Mobile Number.Entered null.");
 		}
 		Pattern pattern = Pattern.compile("[0-9]{1,2}[\s][1-9][0-9]{9}");
 		Matcher matcher = pattern.matcher(mobileNumber);
@@ -50,11 +52,11 @@ public class UserValidator {
 	public boolean validatePassword(String password)throws UserValidationException {
 		try {
 			if (password.length() == 0) {
-				throw new UserValidationException("Please Enter Valid Password. No Password Entered.");
+				throw new UserValidationException(ExceptionType.ENTERED_EMPTY,"Please Enter Valid Password. No Password Entered.");
 			}
 		}
 		catch (NullPointerException e) {
-			throw new UserValidationException("Please Enter Valid Password. Entered null.");
+			throw new UserValidationException(ExceptionType.ENTERED_NULL,"Please Enter Valid Password. Entered null.");
 		}
 		Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$");
 		Matcher matcher = pattern.matcher(password);
