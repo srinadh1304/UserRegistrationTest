e637539029c7afb2567e517cc0ae099042cfb5d4
srinadh1304 <srinadh1304@gmail.com>
2021-09-11 18:54:08 +0530

    ADDED: Enum for exception type

--- lib/src/main/java/com/bridgelabz/userregistrationtest/UserValidationException.java
+++ lib/src/main/java/com/bridgelabz/userregistrationtest/UserValidationException.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.userregistrationtest;
 
 public class UserValidationException extends RuntimeException{
+	
+	ExceptionType type;
+	enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY
+	}
 
-	public UserValidationException(String message) {
+	public UserValidationException(ExceptionType type, String message) {
 		super(message);
+		this.type=type;
 	}
 }
